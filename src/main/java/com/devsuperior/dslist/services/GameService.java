c1a3fb20266bc536c00f3c91a6053c5f9d2551ad
Ruan Silva <ruanluis.s@gmail.com>
2025-02-07 00:46:31 -0300

    Replace List

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -36,7 +36,6 @@ public class GameService {
 	public List<GameMinDTO> findByList(Long listId){	
 	List<GameMinProjection> result = gameRepository.searchByList(listId);	
     return  result.stream().map(x -> new GameMinDTO(x)).toList();
-
 	}
 
 
